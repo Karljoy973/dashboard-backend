472fd47ed94f071ebb992f9d6d86b2bd7f0b07d3
Karl <karljoyeux@gmail.com>
2025-04-01 13:20:31 +0200

    updating cors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  crossOrigin: "anonymous",
   env: {
     DATABASE_URL:
       "postgresql://neondb_owner:npg_K3eRgSPicI5O@ep-nameless-term-aby969cd-pooler.eu-west-2.aws.neon.tech/dashboard-db?sslmode=require",
