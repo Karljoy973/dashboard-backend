08c0a4cc941c4c02d935517aa20c9729ef488c01
Karl <karljoyeux@gmail.com>
2025-04-04 14:04:37 +0200

    patching a security issue

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  env: {
-    DATABASE_URL:
-      "postgresql://neondb_owner:npg_K3eRgSPicI5O@ep-nameless-term-aby969cd-pooler.eu-west-2.aws.neon.tech/dashboard-db?sslmode=require",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
